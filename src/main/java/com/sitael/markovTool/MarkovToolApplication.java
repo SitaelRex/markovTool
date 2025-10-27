f0dd9ac79a4f4584d0d39a4096971be964bf6997
RexSitael <rexsitael@gmail.com>
2025-10-27 19:49:04 +0300

    initial

---
+++ src/main/java/com/sitael/markovTool/MarkovToolApplication.java
@@ -0,0 +1,13 @@
+package com.sitael.markovTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkovToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkovToolApplication.class, args);
+	}
+
+}
