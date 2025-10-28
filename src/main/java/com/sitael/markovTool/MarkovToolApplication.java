47cc2eb9eb8fbd52713e2eca7453136f6f1eff4d
RexSitael <rexsitael@gmail.com>
2025-10-28 18:45:48 +0300

    lua initial

--- src/main/java/com/sitael/markovTool/MarkovToolApplication.java
+++ src/main/java/com/sitael/markovTool/MarkovToolApplication.java
@@ -2,12 +2,21 @@ package com.sitael.markovTool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import party.iroiro.luajava.Lua;
+import party.iroiro.luajava.luajit.LuaJit;
 
 @SpringBootApplication
 public class MarkovToolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarkovToolApplication.class, args);
+
+        try (Lua L = new LuaJit()) {
+            L.set("message", "Hello World from LuaJava");
+            L.run("java.import('java.lang.System').out:println(message)");
+            System.out.println("lua init");
+        }
+
+        SpringApplication.run(MarkovToolApplication.class, args);
 	}
 
 }
