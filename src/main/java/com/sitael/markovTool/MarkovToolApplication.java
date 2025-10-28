0f30bd2335a9848b540929726bb521c9e53347a7
RexSitael <rexsitael@gmail.com>
2025-10-28 19:15:51 +0300

    lua testing

--- src/main/java/com/sitael/markovTool/MarkovToolApplication.java
+++ src/main/java/com/sitael/markovTool/MarkovToolApplication.java
@@ -12,7 +12,8 @@ public class MarkovToolApplication {
 
         try (Lua L = new LuaJit()) {
             L.set("message", "Hello World from LuaJava");
-            L.run("java.import('java.lang.System').out:println(message)");
+            L.openLibraries();
+            L.run("java.import('java.lang.System').out:println(message) for k,v in pairs(_G) do java.import('java.lang.System').out:println(k..'\t'..tostring(v)) if type(v)  == 'table' then for k,v in pairs(v) do java.import('java.lang.System').out:println('\t'..k..'\t'..tostring(v)) end end  end");
             System.out.println("lua init");
         }
 
